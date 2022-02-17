75621bca21fd9e26af8d4669925642c567a93c19
Richa <76250660+Richa-iitr@users.noreply.github.com>
2022-02-17 15:04:01 +0530

    modified

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
-const { API_URL, KEY } = process.env;
+const { API_URL, KEY, PRIVATE_KEY } = process.env;
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -20,7 +20,7 @@ module.exports = {
     },
     matic: {
       url: "https://polygon-mumbai.g.alchemy.com/v2/6OXarysKYq0scJx1uP8RPXxW43Q_eMRj",
-      accounts: [`0x4bf8b687733a18539cc8ed0cb0b59853fd11f1730899f14e9c03dad05098994e`],
+      accounts: [`0x${PRIVATE_KEY}`],
       gas: 2100000,
       gasPrice: 8000000000,
     }
